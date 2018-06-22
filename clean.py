f5d94190bdcb2ccd19ce496c0134ddb8a4766a01
neelshah <neelknightme@gmail.com>
2018-06-22 00:48:29 -0400

    rename label to getsenti and add clean.py

---
+++ clean.py
@@ -0,0 +1,26 @@
+import re
+
+#default setting for data cleaning
+default_settings = {
+	'to_upeer' : False,
+	'to_lower' : False,
+	'remove_special' : False,
+	'remove_number' : False,
+	'spelling_correction' : False,
+	'remove_stopwords' : False,
+	'remove_punchuation' : False, 
+
+}
+
+def clean_it(text, settings):
+
+	return clean_text
+
+
+def test():
+
+	return None
+
+
+if __name__ == '__main__':
+	test()
