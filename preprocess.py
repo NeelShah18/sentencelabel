651e6c4702a0e510dfc50b72fb1ec2f3bb5a9f1c
neelshah <neelknightme@gmail.com>
2018-06-22 19:08:28 -0400

    tokenizer code

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 from textblob import TextBlob
-
+from nltk import word_tokenize
 
 def ngram(text, gram=2):
 	'''convert test to n-gram
@@ -18,11 +18,23 @@ def ngram(text, gram=2):
 	__analyzer = TextBlob(str(text)) 
 	return __analyzer.ngrams(n=gram)
 
-def 
+def tokenizer(text):
+	'''tokenizer return tokens of the text.
+
+	Args:
+		text(str): Text to convert in tokens
+
+	Results:
+		__tokens(list): List of tokens of given text
+
+	'''
+	return word_tokenize(str(text))
+
 
 def test():
 	x = ngram("ever use may thought sat high school english class grown find writing far ever would expected",3)
 	print(x)
+	print(tokenizer("ever use may thought sat high school english class grown find writing far ever would expected"))
 	return None
 
 if __name__ == '__main__':
